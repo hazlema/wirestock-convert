ea254db3118620fa576e16f51e03f53fad54c3f9
Frosty <hazlema@gmail.com>
2023-06-09 19:46:46 -0400

    Version 0.0.2
    Rewrote processing of files to be more efficient.  Added color

--- index.js
+++ index.js
@@ -1,116 +1,71 @@
-/* Batch Convert and Scale your Images
- * Frosty (06/08/23)
- *
- * I use realesrgan-ncnn-vulkan to scale the images (renamed to upscale)
- * https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan
- * 
- * To reprocess an image delete the jpg files
- * 
- * Modifying the scaler
- * --------------------
- * Modify ''const upscale'' to change the scaler command or get 
- * realesrgan-ncnn-vulkan and rename it to upscale
- * 
- * Passed arguments:
- *      {0} is the full path to the input file
- *      {1} is the full path to the output file
- *      {2} is how many times the image should be scaled. (ex. 1, 2, 3, 4)
- *      - Math.floor(Math.log(4194304 / newSize.size)) + 2
- *      - Number of XX times till the resulting size is over 4mb
- */
-const fs       = require('fs');
-const path     = require('path');
-const sharp    = require('sharp');
-const { exec } = require('child_process');
+require('ansi-console-colors')
 
-const directoryPath = process.argv[2] || '.';
-const upscale       = '/usr/bin/upscale -i "{0}" -o "{1}" -s {2} -m "/usr/share/models" -f jpg'
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+const app    = require('./package.json');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 
-var app        = require('./package.json');
-console.log(`\n${app.name}, (${app.version})\n${app.description}\n`)
+// Production
+//const MAX_FILE_SIZE = 4 * 1024 * 1024; // 4MB
 
-async function convertJpeg(inFile, outFile) {
-    await sharp(inFile).toFormat('jpeg').toFile(outFile);
-    return Promise.resolve();
+// Testing
+const MAX_FILE_SIZE = 796620
+
+const upscaleCmd    = '/usr/bin/upscale -i "{input}" -o "{output}" -s {factor} -m "/usr/share/models" -f jpg'
+
+function replaceTokens(string, replacements) {
+    const regex = new RegExp(Object.keys(replacements).join('|'), 'g');
+    return string.replace(regex, (matched) => replacements[matched]);
 }
 
-async function execScale(cmd) {
-    await exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            throw('Error executing scaler shell command:', error);
-        }
-    })
-    return Promise.resolve();
-}
+async function convertResize(directoryPath) {
+    var convertCount = 0
+    var publishCount = 0
 
-fs.readdir(directoryPath, async (error, files) => {
-    if (error) {
-        throw('Error reading directory:', error);
-    }
-    
-    // Filter Array
-    let filtered = files.filter(files => {
-        return /\.(png|tif|tiff|gif|svg|raw)$/i.test(files)
-    })
-
-    if (filtered.length == 0) {
-        console.log("Nothing to process, supported extensions: png, tiff, tif, gif, svg, raw")
-    }
-
-    for (const file of filtered) {
-        // File Details
-        let process  = path.join(directoryPath, file);
-        let fileExt  = path.extname(process);
-        let fileName = path.basename(process, fileExt);
-        let filePath = path.dirname(process);
+    try {
+        const files = await fs.promises.readdir(directoryPath);
         
-        // File Paths
-        let fileJpg       = `${fileName}.jpg`;
-        let fileJpgPath   = path.join(filePath, fileJpg);
-        let fileScalePath = path.join(filePath, '000-pub-' + fileName + '.jpg');
-        
-        console.log(`Processing (${filePath}, ${fileName}, ${fileExt})`);
-            
-        // Do I need to create a jpeg?
-        const jpgExists = files.some((file) => {
-            return file.toLowerCase() === fileJpg.toLowerCase();
-        });
-            
-        // Create a jpeg
-        if (!jpgExists) {
-            console.log(`Converting to ${fileJpgPath}`);
-            await convertJpeg(process, fileJpgPath);
-        }
-            
-        // Do I need to create an upscale?
-        const scaleExists = files.some((file) => {
-            return (file.toLowerCase() === fileScalePath.toLowerCase());
-        });
+        for (const file of files) {
+            if (/^(?!111-stage-|000-publish-)(.*)(\.(png|jpg|jpeg|tif|tiff|gif|svg|raw)$)/i.test(file)) {
+                const filePath      = path.join(directoryPath, file);
+                const outputFile    = path.parse(file).name
+                const outputFileJpg = path.join(directoryPath, `111-stage-${outputFile}.jpg`);
+                const outputFilePub = path.join(directoryPath, `000-publish-${outputFile}.jpg`);
                 
-        // Upscale
-        if (!scaleExists) {
-            const newSize = await fs.promises.stat(process);
-                   
-            // Copy JPG to Published
-            if (newSize.size >= 4194304) {
-                console.log(`No scaling needed on ${fileJpgPath}`);
-                fs.copyFile(fileJpgPath, fileScalePath, (err) => {
-                    if (err) throw err;
-                });
-                console.log(`Published ${fileJpgPath} -> ${fileScalePath}`);
-            } else {
-                // Upscale
-                var resize = Math.floor(Math.log(4194304 / newSize.size)) + 2;
-                console.log(`Scale ${fileJpgPath} x`, resize);
-                            
-                let cmd = upscale;
-                cmd = cmd.replace('{0}', fileJpgPath);
-                cmd = cmd.replace('{1}', fileScalePath);
-                cmd = cmd.replace('{2}', resize);
-                await execScale(cmd)
-                console.log(`Published ${fileJpgPath} -> ${fileScalePath}`);
+                if (!fs.existsSync(outputFileJpg)) {
+                    convertCount += 1
+                    console.print(`@X0BConverting @X0F${file} @X0B-> @X0F${outputFileJpg}`);
+                    await sharp(filePath).toFormat('jpeg').toFile(outputFileJpg);
+                }
+                
+                if (!fs.existsSync(outputFilePub)) {
+                    publishCount += 1
+                    const { size } = await fs.promises.stat(outputFileJpg);
+                    if (size <= MAX_FILE_SIZE) {
+                        const resizeCommand = replaceTokens(upscaleCmd, {
+                            '{input}':  outputFileJpg,
+                            '{output}': outputFilePub,
+                            '{factor}': Math.floor(Math.log(MAX_FILE_SIZE / size))
+                        })
+                        
+                        console.print(`@X0BResizing @X0F${outputFilePub} @X0B/ @X0F${size} @X0Bbytes`);
+                        await exec(resizeCommand);
+                    } else {
+                        console.print(`@X0BCopying @X0F${outputFileJpg} @X0B/ @X0F${size} @X0Bbytes -> @X0F${outputFilePub}@X0B, @X0C(exceeds ${MAX_FILE_SIZE})`);
+                        await fs.promises.copyFile(outputFileJpg, outputFilePub)
+                    }
+                }
             }
         }
         console.log()
+        console.print(`@X0BFiles Converted: @X0F${convertCount}`);
+        console.print(`@X0BFiles Published: @X0F${publishCount}`);
+    } catch (error) {
+        console.error('Error:', error);
     }
-});
+}
+
+console.print(`@X0B${app.name}, (@X0Cv@X0F${app.version}@X0B)\n@X0E${app.description}\n`)
+convertResize(process.argv[2] || '.');
